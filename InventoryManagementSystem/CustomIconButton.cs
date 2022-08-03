b4202275f0105d1b78c757530b5f89b7e03d7320
ElHanaya <89678375+phatnguyen1710@users.noreply.github.com>
2022-08-03 14:27:14 +0700

    Add custom Icon

---
+++ InventoryManagementSystem/CustomIconButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagementSystem
+{
+    public partial class CustomIconButton : PictureBox
+    {
+        private Image normalImage;
+        private Image hoverImage;
+        public CustomIconButton()
+        {
+            InitializeComponent();
+        }
+
+        public Image ImageNormal { get => normalImage; set => normalImage = value; }
+        public Image ImageHover { get => hoverImage; set => hoverImage = value; }
+
+        private void CustomIconButton_MouseHover(object sender, EventArgs e)
+        {
+            this.Image = ImageHover;
+        }
+
+        private void CustomIconButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.Image = ImageNormal;
+        }
+    }
+}
